a5f3be8460de4a26fe7bc820c701e2c3ab2337f0
sxblde <svn.sxblde@gmail.com>
2024-10-04 16:24:35 +1000

    Add files via upload

---
+++ css/main.js
@@ -0,0 +1,8 @@
+const parallax = document.getElementById ("hero-top");
+
+window.addEventListener("scroll", function ()
+{
+let offset = window.pageYOffset;
+console.log('Offset: ' + offset);
+console.log('offset * 0.7 + offset *0.7')
+})
